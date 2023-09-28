940c3f07e38de73439c0d7d16ac1b983ef8ed3a9
孙铁义 <zxc986977978@qq.com>
2023-09-28 16:21:30 +0800

    解决权限失效
    Former-commit-id: 59bf399c273f7d84ae0a460ea85607d0f0c40303

--- src/main/java/game605/configClass/WebConfig.java
+++ src/main/java/game605/configClass/WebConfig.java
@@ -1,4 +1,46 @@
 package game605.configClass;
 
-public class WebConfig {
+import game605.Interceptors.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public ImgInterceptor getImgInter(){
+        return new ImgInterceptor();
+    }
+
+    @Bean
+    public TagInterceptor getTagInter(){
+        return new TagInterceptor();
+    }
+
+    @Bean
+    public UserInterceptor getUserInter(){
+        return new UserInterceptor();
+    }
+
+    @Bean
+    public RoleInterceptor getRoleInter(){
+        return new RoleInterceptor();
+    }
+
+    @Bean
+    public AuthInterceptor getAuthInter(){
+        return new AuthInterceptor();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        // registry.addInterceptor(new AllInterceptor());
+        // registry.addInterceptor(getUserInter()).addPathPatterns("/user/*");
+        registry.addInterceptor(getRoleInter()).addPathPatterns("/role/*");
+        registry.addInterceptor(getAuthInter()).addPathPatterns("/auth/*");
+        registry.addInterceptor(getImgInter()).addPathPatterns("/img/*");
+        registry.addInterceptor(getTagInter()).addPathPatterns("/tag/*");
+    }
 }
