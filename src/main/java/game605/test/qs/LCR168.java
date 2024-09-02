3e1530f983ee7fc78cdca0e73c2178d1e890b199
孙铁义 <zxc986977978@qq.com>
2024-09-02 18:29:52 +0800

    刷题
    Former-commit-id: e3fcd9a043a9273aabcebfb7f9a83316070fc383

--- src/main/java/game605/test/qs/LCR168.java
+++ src/main/java/game605/test/qs/LCR168.java
@@ -9,6 +9,17 @@ package game605.test.qs;
  */
 public class LCR168 {
 
-
+    public int nthUglyNumber(int n) {
+        int[] dp = new int[n + 1];
+        int p2=1, p3=1, p5=1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = Math.min(Math.min(dp[p2]*2, dp[p3]*3), dp[p5]*5);
+            if (dp[i] == dp[p2]*2) p2++;
+            if (dp[i] == dp[p3]*3) p3++;
+            if (dp[i] == dp[p5]*5) p5++;
+        }
+        return dp[n];
+    }
 
 }
